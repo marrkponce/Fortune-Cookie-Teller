3853a75de8589b171a19a8f882fdd874950b2ecd
marrkponce <markponce14@live.com>
2023-04-25 21:55:13 -0700

    Second button click resets page and have 25 fortune messages

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.getElementById('crack').addEventListener('click', crackOpen);
 
 const fortuneGenerator = () => {
     
-    let randomNumber = Math.floor(Math.random() * 10);
+    let randomNumber = Math.floor(Math.random() * 25);
     let num = randomNumber
     
     switch(num){
@@ -34,7 +34,52 @@ const fortuneGenerator = () => {
             return 'Do not hesitate to pick a difficult problem';
             break;
         case 9:
-            return 'You have a good reason to be self confident'
+            return 'You have a good reason to be self confident';
+            break;
+        case 10:
+            return 'True happiness makes us wise.';
+            break;
+        case 11:
+            return 'You can learn much from people who are different from you.';
+            break;
+        case 12:
+            return 'Focus on your health and well-being now.';
+            break;
+        case 13:
+            return 'Do not let ambitions overshadow small success.';
+            break;
+        case 14:
+            return 'Do not just think, act!';
+            break;
+        case 15:
+            return 'Expect much of yourself and little of others.';
+            break;
+        case 16:
+            return 'Good to begin well, better to end well.';
+            break;
+        case 17:
+            return 'It is better to be an optimist and proven a fool than to be a pessimist and be proven right.';
+            break;
+        case 18:
+            return 'Love is a warm fire to keep the soul warm.';
+            break;
+        case 19:
+            return 'Physical activity will dramatically improve your outlook today.';
+            break;
+        case 20:
+            return 'Remember the birthday but never the age.';
+            break;
+        case 21:
+            return 'Success is failure turned inside out.';
+            break;
+        case 22:
+            return 'The harder you work, the luckier you get.';
+            break;
+        case 23:
+            return 'To know oneself, one should assert oneself.'
+            break;
+        case 24:
+            return 'Your ability is appreciated.';
             break;
         default: 
             return 'Try Again'
@@ -42,5 +87,17 @@ const fortuneGenerator = () => {
 } 
 
 function crackOpen(){
-    document.getElementById('cookie').innerHTML = fortuneGenerator()
+    
+    const fortuneDisplay = document.getElementById('cookie');
+    const fortune = fortuneGenerator();
+
+   
+    if (fortuneDisplay.innerHTML !== '') {
+        fortuneDisplay.innerHTML = '';
+      }else{
+        fortuneDisplay.innerHTML = fortune;
+
+      }
+
 }
+
